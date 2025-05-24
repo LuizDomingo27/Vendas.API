1c33f30e0f1d081507ba3bfd8449b0fef61df339
LuizDomingo27 <luiz-elayny@live.com>
2025-05-24 02:33:18 -0300

    New correcao na rota

--- Vendas.API/Infra/EmployesContext.cs
+++ Vendas.API/Infra/EmployesContext.cs
@@ -7,25 +7,25 @@ namespace Vendas.API.Infra;
 
 public class EmployesContext : DbContext
 {
-	//Criamos o construtor
-	public EmployesContext(DbContextOptions<EmployesContext> options) : base(options) { }
+  //Criamos o construtor
+  public EmployesContext(DbContextOptions<EmployesContext> options) : base(options) { }
 
-	//Criamos o nosso DbSet<>
-	public DbSet<Employes> Employes { get; set; }
-	public DbSet<RegisterUsers> RegisterUsers { get; set; }
+  //Criamos o nosso DbSet<>
+  public DbSet<Employes> Employes { get; set; }
+  public DbSet<RegisterUsers> RegisterUsers { get; set; }
 
-	//Sobrescrevemos o metodo OnModelCreating()
-	protected override void OnModelCreating(ModelBuilder modelBuilder)
-	{
-		//Aqui estou afirmando para não ter chave primaria
-		modelBuilder.Entity<Employes>(e => e.HasNoKey());
-		modelBuilder.Entity<RegisterUsers>(e => e.HasNoKey());
+  //Sobrescrevemos o metodo OnModelCreating()
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    //Aqui estou afirmando para não ter chave primaria
+    modelBuilder.Entity<Employes>(e => e.HasNoKey());
+    modelBuilder.Entity<RegisterUsers>(e => e.HasNoKey());
 
-		//Para criar as tabelas em nosso banco de dados precisamos adicionar o mapeamento
-		modelBuilder.ApplyConfiguration(new EmployesMap());
-		modelBuilder.ApplyConfiguration(new RegisterUserMap());
+    //Para criar as tabelas em nosso banco de dados precisamos adicionar o mapeamento
+    modelBuilder.ApplyConfiguration(new EmployesMap());
+    modelBuilder.ApplyConfiguration(new RegisterUserMap());
 
-		//Criando as configurações
-		base.OnModelCreating(modelBuilder);
-	}
+    //Criando as configurações
+    base.OnModelCreating(modelBuilder);
+  }
 }
--- Vendas.API/Infra/JwtTokenGenerator.cs
+++ Vendas.API/Infra/JwtTokenGenerator.cs
@@ -10,37 +10,37 @@ namespace Vendas.API.Infra;
 
 public class JwtTokenGenerator : IGeneratorToken
 {
-	private readonly uint _expirationTimeOrDate;
-	private readonly string? _secretKey;
+  private readonly uint _expirationTimeOrDate;
+  private readonly string? _secretKey;
 
-	public JwtTokenGenerator(uint expirationTimeOrDate, string? secretKey)
-	{
-		_expirationTimeOrDate = expirationTimeOrDate;
-		_secretKey = secretKey;
-	}
+  public JwtTokenGenerator(uint expirationTimeOrDate, string? secretKey)
+  {
+    _expirationTimeOrDate = expirationTimeOrDate;
+    _secretKey = secretKey;
+  }
 
-	public string GenerateToken(Guid userIdentier)
-	{
-		List<Claim> claim =
-		[
-			new Claim(ClaimTypes.Sid, userIdentier.ToString()),
-		];
+  public string GenerateToken(Guid userIdentier)
+  {
+    List<Claim> claim =
+    [
+      new Claim(ClaimTypes.Sid, userIdentier.ToString()),
+    ];
 
-		var tokenDescriptor = new SecurityTokenDescriptor
-		{
-			Subject = new ClaimsIdentity(claim),
-			Expires = DateTime.UtcNow.AddMinutes(_expirationTimeOrDate),
-			SigningCredentials = new SigningCredentials(SecurityKey(), SecurityAlgorithms.HmacSha256Signature),
-		};
+    var tokenDescriptor = new SecurityTokenDescriptor
+    {
+      Subject = new ClaimsIdentity(claim),
+      Expires = DateTime.UtcNow.AddMinutes(_expirationTimeOrDate),
+      SigningCredentials = new SigningCredentials(SecurityKey(), SecurityAlgorithms.HmacSha256Signature),
+    };
 
-		var tokenHanlder = new JwtSecurityTokenHandler();
-		var securityToken = tokenHanlder.CreateToken(tokenDescriptor);
-		return tokenHanlder.WriteToken(securityToken);
-	}
+    var tokenHanlder = new JwtSecurityTokenHandler();
+    var securityToken = tokenHanlder.CreateToken(tokenDescriptor);
+    return tokenHanlder.WriteToken(securityToken);
+  }
 
-	private SymmetricSecurityKey SecurityKey()
-	{
-		byte[] bytes = Encoding.UTF8.GetBytes(_secretKey!);
-		return new SymmetricSecurityKey(bytes);
-	}
+  private SymmetricSecurityKey SecurityKey()
+  {
+    byte[] bytes = Encoding.UTF8.GetBytes(_secretKey!);
+    return new SymmetricSecurityKey(bytes);
+  }
 }
